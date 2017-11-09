27374c1f8e53cd62ac2eef15ceef6317b4522081
Marcin Radziszewski <M.Radziszewski@hypermedia.pl>
2017-11-09 11:18:58 +0100

    Add project files.

---
+++ tests/Models/DbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace tests.Models
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options)
+                : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
